5d5199069151aecd84a329f68b3252508d8fda5a
Newbclharri <contactharrisc2@gmail.com>
2022-08-12 18:15:55 -0600

    debugging

--- App.js
+++ App.js
@@ -2,9 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 1;
+  console.log('App executed');
+  function handlePress(){
+    console.log("Text 1 pressed");
+  };
+
+  handlePress();
   return (
     <View style={styles.container}>
-      <Text>Finally!</Text>
+      <Text onPress={handlePress}>Finally, it works, again!</Text>
       <StatusBar style="auto" />
     </View>
   );
